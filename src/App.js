4321879b7eacfc515072f0693bcfee204440121d
Solem Tchangai <stchangai@atd16.lan>
2019-04-08 17:52:27 +0200

    essai 4 index.html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Admin, Resource } from 'react-admin';
+import simpleRestProvider from 'ra-data-simple-rest';
 import Dashboard from './Dashboard.js';
 
 const App = () => (
     <Admin dashboard={Dashboard} dataProvider={dataProvider}>
         // ...
     </Admin>
-  );
-import React from 'react';
-import { render } from 'react-dom';
-import { Admin, Resource } from 'react-admin';
-import simpleRestProvider from 'ra-data-simple-rest';
+);
