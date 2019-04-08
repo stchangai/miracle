0f5413d1556011d782b5d45b892b1957bb61e65a
Solem Tchangai <stchangai@atd16.lan>
2019-04-08 17:30:54 +0200

    essai index.html

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import { render } from 'react-dom';
 import { Admin, Resource } from 'react-admin';
 import simpleRestProvider from 'ra-data-simple-rest';
 
-import { PostList, PostEdit, PostCreate, PostIcon } from './posts';
+//import { PostList, PostEdit, PostCreate, PostIcon } from './posts';
 import users from './users';
-import sctructures from './structures';
-import Dashboard from './Dashboard';
+//import sctructures from './structures';
+import Dashboard from './Dashboard.js';
 
 const App = () => (
     <Admin dashboard={Dashboard} dataProvider={dataProvider}>
         // ...
     </Admin>
-    
+
 render(
     <Admin dataProvider={simpleRestProvider('http://localhost:3000')}>
         <Resource name=users" list={UserList} edit={USerEdit} create={UserCreate} icon={USerIcon}/>
