676c9a311c5181b67c9cf0591fa38fe024bbb5ad
Solem Tchangai <stchangai@atd16.lan>
2019-04-08 16:14:05 +0200

    mise en place page d'accueil

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Admin, Resource } from 'react-admin';
+import simpleRestProvider from 'ra-data-simple-rest';
+
+import { PostList, PostEdit, PostCreate, PostIcon } from './posts';
+import users from './users';
+import sctructures from './structures';
+import Dashboard from './Dashboard';
+
+const App = () => (
+    <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+        // ...
+    </Admin>
+    
+render(
+    <Admin dataProvider={simpleRestProvider('http://localhost:3000')}>
+        <Resource name=users" list={UserList} edit={USerEdit} create={UserCreate} icon={USerIcon}/>
+    </Admin>,
+    document.getElementById('root')
+);
